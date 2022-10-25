c67d8321e044bc99f8cea8a1ba88fbd1ee55a887
Uzair <uzair.t2006@gmail.com>
2022-10-25 17:20:35 +0500

    remove search

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,10 +21,6 @@ function Navbar() {
               <Link className="nav-link active" aria-current="page" to="/product">Product</Link>
             </li>
           </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-light" type="submit">Search</button>
-          </form>
         </div>
       </div>
     </nav>
